9b15b950dbf4e0fa9d91d974124830f337e08598
kosta159159@gmail.com <kosta159159@gmail.com>
2021-10-08 20:09:39 +0300

    added invites & repository model

---
+++ ConferenceApp/Models/Invites.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Models
+{
+    public class Invites
+    {
+        [Required(ErrorMessage = "Please enter name...")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter email...")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter phone number...")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you will join")]
+        public bool? WillJoin { get; set; }
+    }
+}
---
+++ ConferenceApp/Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Models
+{
+    public static class Repository
+    {
+        private static List<Invites> responses = new List<Invites>();
+
+        public static IEnumerable<Invites> Responses = responses;
+
+        public static void AddResponse(Invites response)
+        {
+            responses.Add(response);
+        }
+    }
+}
